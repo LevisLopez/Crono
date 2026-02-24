da00ea7dff87d21a979c0181d9707d88c2a27ad5
LevisLopez <96857417+LevisLopez@users.noreply.github.com>
2026-02-24 11:42:45 -0500

    Update service worker cache version to v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moto-speedometer-v11';
+const CACHE_NAME = 'moto-speedometer-v12';
 const urlsToCache = [
   './index.html',
   './manifest.json',
@@ -33,6 +33,7 @@ self.addEventListener('fetch', (event) => {
       .catch(() => new Response('Offline.', { headers: { 'Content-Type': 'text/plain' } }))
   );
 });
+
 
 
 
